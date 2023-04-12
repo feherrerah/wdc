2ee94bef417e0be9004839ae93437f81bc1aa331
feherrerah <Fabian.Herrera@kinesso.com>
2023-04-11 19:41:35 -0500

    adding files

---
+++ app.js
@@ -0,0 +1,9 @@
+(function(){
+    var myConnector = tableau.makeConnector();
+
+    myConnector.getSchema = function(schemaCallback){};
+
+    myConnector.getData = function(table, doneCallback){};
+
+    tableau.registerConnector(myConnector);
+})();
